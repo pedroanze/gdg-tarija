809df51f2dcbe8e1cd7796cd04164d1d34ff4c96
Amilkar Dominguez <amilkar.dominguez@gmail.com>
2024-06-24 00:57:41 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [tailwind()],
   site: 'https://amilkardominguez.github.io',
   base: 'gdg-tarija',
 });
