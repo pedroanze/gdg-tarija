1f4c73e7cdb8c74fb9471ac152e78b19235b6fdd
anze28 <pedro2528anze@gmail.com>
2024-06-27 13:10:57 -0400

    rutas domain: prueba 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: 'https://amilkardominguez.github.io',
-  base: 'gdg-tarija',
-  integrations: [tailwind()]
+  site: 'https://gdgtarija.com',  
+  base: '/', 
+  integrations: [tailwind()],
 });
