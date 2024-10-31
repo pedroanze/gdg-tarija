912e311b4908afbe5681b16b5a45e9665cf35a81
anze28 <pedro2528anze@gmail.com>
2024-10-31 15:25:56 -0400

    fix: paquetes

--- src/components/componentsDevFest2024/packages/helpers/cardDevFest2024.ts
+++ src/components/componentsDevFest2024/packages/helpers/cardDevFest2024.ts
@@ -2,6 +2,7 @@ export class CardDevFest2024 {
   title: string;
   packageElements: string[];
   price: number;
+  content: ImageMetadata;
   cardImage?: ImageMetadata;
   cardColor: 'blue' | 'green';
   link: string;
@@ -10,6 +11,7 @@ export class CardDevFest2024 {
       title = '',
       packageElements = [],
       price = 0,
+      content = undefined,
       cardImage = undefined,
       cardColor = 'blue',
       link= '',
@@ -18,6 +20,7 @@ export class CardDevFest2024 {
     this.title = title;
     this.packageElements = packageElements;
     this.price = price;
+    this.content = content;
     this.cardImage = cardImage;
     this.cardColor = cardColor;
     this.link = link;
--- src/components/componentsDevFest2024/packages/helpers/devCards.ts
+++ src/components/componentsDevFest2024/packages/helpers/devCards.ts
@@ -1,17 +1,24 @@
-import PackagePhoto from './../../../../assets/devfest2024/svg/PackagePhoto.svg';
+
 import type { CardDevFest2024 } from './cardDevFest2024';
+import PaqueteGoldDevFest from "./../../../../assets/devfest2024/img/paqueteGoldImg.png";
+import PaqueteSilverDevFest from "./../../../../assets/devfest2024/img/paqueteSilverImg.png";
+import ContentSilver from "./../../../../assets/devfest2024/svg/contentSiver.svg"
+import ContentGold from "./../../../../assets/devfest2024/svg/contentGold.svg"
 
 export const devCards: CardDevFest2024[] = [
   {
-    title: "Paquete Developer",
+    title: "Paquete Silver",
     packageElements: ["2 Stickers", "1 Llavero", "1 Cuaderno", "2 Pines"],
-    price: 30,
-    cardImage: PackagePhoto,
+    content: ContentSilver,
+    
+    price: 25,
+    cardImage: PaqueteSilverDevFest,
+    
     cardColor: "blue",
-    link: '',
+    link: 'https://forms.gle/H3NY8vyjrwshj4hW6',
   },
   {
-    title: "Paquete ML",
+    title: "Paquete Gold",
     packageElements: [
       "3 Stickers",
       "1 Llavero",
@@ -19,9 +26,10 @@ export const devCards: CardDevFest2024[] = [
       "2 Pines",
       "1 Vaso con Popote",
     ],
-    price: 50,
-    cardImage: PackagePhoto,
+    price: 45,
+    content: ContentGold,
+    cardImage: PaqueteGoldDevFest,
     cardColor: "green",
-    link: '',
+    link: 'https://forms.gle/H3NY8vyjrwshj4hW6',
   },
 ];
