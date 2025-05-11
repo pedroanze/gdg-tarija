b8c0b6526300353a2282b3b1fc0b7cd53518dcc5
pedroanze <pedro2528anze@gmail.com>
2025-05-11 11:33:07 -0400

    feat: Update tutorials and add new navbar component
    - Added new tutorial on "Model Context Protocol con Gemini" with detailed sections and subsections.
    - Updated package.json to include new dependencies for icons and assets.
    - Refactored tutorial page layout to include a new Navbar component for better navigation.
    - Removed deprecated tutorialPage.astro file.
    - Created a responsive Navbar component with menu items for GDG Tarija and Build With AI.
    - Enhanced styling and structure for improved user experience across tutorial pages.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
+import icon from 'astro-icon';
+
 export default defineConfig({
   site: 'https://gdgtarija.com',  
   base: '/', 
-  integrations: [tailwind()],
+  integrations: [tailwind(), icon()],
 });
